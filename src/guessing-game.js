8ec2104d052f320c85137ff08343fe296e6fa7f5
DevDimon <devdimonpok@gmail.com>
2024-09-19 17:41:59 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minRangeValue = 0;
+        this.maxRangeValue = 0;
+        this.resultValue = 0;
+    }
 
     setRange(min, max) {
-
+        this.minRangeValue = min;
+        this.maxRangeValue = max;
     }
 
     guess() {
-
+        this.resultValue = Math.round((this.minRangeValue + this.maxRangeValue) / 2);
+        return this.resultValue;
     }
 
     lower() {
-
+        this.maxRangeValue = this.resultValue;
     }
 
     greater() {
-
+        this.minRangeValue = this.resultValue;
     }
 }
 
